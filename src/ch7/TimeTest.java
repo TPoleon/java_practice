b469d6f98142b660b9fff95b1594df14d02d4065
TPOLEON\tmshd <tmshdnzmfoq@gmail.com>
2023-12-08 17:28:20 +0900

    2023/12/08

--- src/ch7/TimeTest.java
+++ src/ch7/TimeTest.java
@@ -2,7 +2,7 @@ package ch7;
 
 public class TimeTest {
     public static void main(String[] args) {
-        Time t = new Time(12, 35, 30);
+        Time t = new Time(16, 35, 30);
         System.out.println(t);
         //t.hour; 변수 hour의 접근제어자가 private이므로 접근할 수 없다.
         t.setHour(t.getHour() + 1);   // 현재시간보다 1시간 후로 변경한다.
